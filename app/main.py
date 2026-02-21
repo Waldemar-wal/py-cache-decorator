5431b1837ad402dde328da3025346aae2e4324f2
Waldemarr <sava.v80@gmail.com>
2026-02-21 14:38:51 +0200

    fix function wrapper

--- app/main.py
+++ app/main.py
@@ -7,13 +7,13 @@ def cache(func: Callable) -> Callable:
     cache_store = {}
 
     @wraps(func)
-    def wrapper(*args):
-        key = args
+    def wrapper(*args, **kwargs):
+        key = args, kwargs
         if key in cache_store:
             print("Getting from cache")
             return cache_store[key]
         print("Calculating new result")
-        result = func(*args)
+        result = func(*args, **kwargs)
         cache_store[key] = result
         return result
 
